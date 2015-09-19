6a58cb3e3ba054e0faa64a5f696720c417ca5297
Felix Sun <fephsun@gmail.com>
2015-09-19 14:11:04 -0400

    Loggiing???

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from flask import Flask
 # import twilio.twiml
- 
+
 app = Flask(__name__)
+
+import logging
+file_handler = logging.FileHandler("log.txt")
+file_handler.setLevel(logging.WARNING)
+app.logger.addHandler(file_handler)
+ 
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
