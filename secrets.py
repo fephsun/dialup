27e063aac097c80a386a9854fed20f93a130228d
Chelsea Voss <csvoss@mit.edu>
2015-09-19 19:40:52 -0400

    Support Heroku's method of getting secrets

---
+++ secrets.py
@@ -0,0 +1,8 @@
+is_dev_server = True
+
+if is_dev_server:
+    from actual_secrets import *
+else:
+    import os
+    clarifai_access_token = os.environ['clarifai_access_token']
+    witai_api_key = os.environ['witai_api_key']
