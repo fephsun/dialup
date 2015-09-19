be8e2169d872959dbb970564ec14f7721cf92522
Felix Sun <fephsun@gmail.com>
2015-09-19 19:13:01 -0400

    Finding a file upload place

--- run.py
+++ run.py
@@ -75,7 +75,7 @@ def record():
     this_user = User.query.filter_by(userid=userid).first()
 
     audio = request.files['filename'].read()
-    out_file = file('~/test.wav', 'wb')
+    out_file = file('./test.wav', 'wb')
     this_user.voice_query = audio
     out_file.write(audio)
     db.session.commit()
